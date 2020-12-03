c717a248fb37a41507353d0ad23796c5b1f01e8e
AbdullahQureshi <adnx1080@gmail.com>
2020-12-03 23:07:17 +0500

    basic setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const app = express();
+
+// Import Routes
+const authRoute = require("./routes/auth");
+
+
+dotenv.config();
+
+// DB connect 
+mongoose.connect(
+process.env.DB_CONNECT,
+{useNewUrlParser: true,
+useUnifiedTopology:true,
+},
+
+()=>{ console.log("Connected to DB !");})
+
+
+
+// Middlewires
+app.use(express.json());
+
+app.use('/api/user', authRoute);
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
+})
